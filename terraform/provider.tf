0b03d26741abdc9542c45722df7e4000e47d1da4
Kiranjith <kiranjith83@gmail.com>
2024-04-15 12:00:36 +1000

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,13 +4,5 @@ provider "aws" {
 }
 # set the backend as s3
 terraform {
-  required_version = ">= 0.12"
-  required_providers {
-    aws = ">= 2.7.0"
-  }
-  backend "s3" {
-    bucket = "myairflowlionkiran"
-    key    = "terraform.tfstate"
-    region = "ap-southeast-2"
-  }
+  backend "s3" {}
 }
