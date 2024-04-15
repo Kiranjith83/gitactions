b88bbf5ced75d506e0d04bae62c7282747fa68a4
Kiranjith <kiranjith83@gmail.com>
2024-04-15 11:21:20 +1000

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,21 +1,17 @@
 terraform {
+  required_version = ">= 1.6"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      Version = "~>3.27"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
-
-    required_version = ">=0.14.9" 
-
-   backend "s3" {
-       bucket = "myairflowlionkiran"
-       key    = "myTFstatefile"
-       region = "ap-southeast-2"
-   }
-}
-
-provider "aws" {
-  version = "~>3.0"
-  region  = "ap-southeast-2"
+  backend "s3" {
+    # bucket         = "proserve-anz-pyfluit-tf-statefiles-20240410"
+    # key            = "PyfluitStateFile"
+    bucket = "myairflowlionkiran"
+    key    = "myTFstatefile"
+    region         = "ap-southeast-2"
+    dynamodb_table = "proserve-anz-pyfluit-tf-statefiles"
+  }
 }
